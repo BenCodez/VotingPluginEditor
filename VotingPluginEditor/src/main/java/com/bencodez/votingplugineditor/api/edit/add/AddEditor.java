53de31946a78f2af342ad35d26db483c38f72d49
BenCodez <benbergen12@gmail.com>
2025-02-04 14:24:54 -0600

    Improve look of adding/remove votesites,etc

--- VotingPluginEditor/src/main/java/com/bencodez/votingplugineditor/api/edit/add/AddEditor.java
+++ VotingPluginEditor/src/main/java/com/bencodez/votingplugineditor/api/edit/add/AddEditor.java
@@ -1,91 +1,43 @@
 package com.bencodez.votingplugineditor.api.edit.add;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane;
 
-import com.bencodez.votingplugineditor.api.settng.SettingButton;
+import com.bencodez.votingplugineditor.PanelUtils;
 
 public abstract class AddEditor {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JComboBox<String> comboBox;
-	private List<String> options;
 
 	public AddEditor(String name) {
+		open(name, null);
+	}
 
-		// Create GUI
-		createAndShowGUI(name);
+	private void open(String name, List<String> options) {
+
+		String input = "";
+		if (options != null && options.size() > 1) {
+
+			Object obj = JOptionPane.showInputDialog(frame, name, "Confirm", JOptionPane.PLAIN_MESSAGE, null,
+					PanelUtils.convertListToArray(options), options.get(0));
+			if (obj != null) {
+				input = obj.toString();
+			}
+		} else {
+			Object obj = JOptionPane.showInputDialog(frame, name, "Confirm", JOptionPane.PLAIN_MESSAGE, null, null, "");
+			if (obj != null) {
+				input = obj.toString();
+			}
+		}
+		if (input != null && !input.isEmpty()) {
+			onAdd(input);
+		}
 	}
 
 	public AddEditor(String name, List<String> options) {
-		this.options = options;
-
-		// Create GUI
-		createAndShowGUI(name);
-	}
-
-	private void createAndShowGUI(String name) {
-		frame = new JFrame(name);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setSize(600, 600);
-		frame.setLayout(new BorderLayout());
-
-		JPanel panel = createMainPanel();
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
-
-	private JPanel createMainPanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-
-		JPanel addPanel = new JPanel();
-		addPanel.setLayout(new BoxLayout(addPanel, BoxLayout.X_AXIS));
-
-		JLabel label = new JLabel("Name:");
-		addPanel.add(label);
-
-		if (options != null && !options.isEmpty()) {
-			comboBox = new JComboBox<>();
-			comboBox.setMaximumSize(new Dimension(Integer.MAX_VALUE, comboBox.getPreferredSize().height));
-			for (String option : options) {
-				comboBox.addItem(option);
-			}
-			addPanel.add(comboBox);
-		} else {
-			textField = new JTextField("");
-			textField.setMaximumSize(new Dimension(Integer.MAX_VALUE, textField.getPreferredSize().height));
-			addPanel.add(textField);
-		}
-
-		panel.add(addPanel);
-
-		JButton addButton = new JButton("Click to Add");
-		addButton.addActionListener(event -> {
-			if (comboBox != null) {
-				onAdd((String) comboBox.getSelectedItem());
-			} else {
-				onAdd(textField.getText());
-			}
-			frame.dispose();
-		});
-		panel.add(addButton);
-
-		return panel;
+		open(name, options);
 	}
 
 	public abstract void onAdd(String name);
--- VotingPluginEditor/src/main/java/com/bencodez/votingplugineditor/api/edit/add/RemoveEditor.java
+++ VotingPluginEditor/src/main/java/com/bencodez/votingplugineditor/api/edit/add/RemoveEditor.java
@@ -1,71 +1,38 @@
 package com.bencodez.votingplugineditor.api.edit.add;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.util.Arrays;
+import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
 public abstract class RemoveEditor {
 
 	private JFrame frame;
 
-	private String[] options;
+	private void open(String name, List<String> options) {
 
-	public RemoveEditor(String name, String[] options) {
-		this.options = options;
-		// Create GUI
-		createAndShowGUI(name);
+		String input = "";
+
+		Object obj = JOptionPane.showInputDialog(frame, name, "Confirm Remove", JOptionPane.PLAIN_MESSAGE, null,
+				options.toArray(), options.get(0));
+		if (obj != null) {
+			input = obj.toString();
+		}
+
+		if (input != null && !input.isEmpty()) {
+			onRemove(input);
+		}
 	}
 
-	private void createAndShowGUI(String name) {
-		frame = new JFrame(name);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setSize(600, 600);
-		frame.setLayout(new BorderLayout());
-
-		JPanel panel = createMainPanel();
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+	public RemoveEditor(String name, List<String> options) {
+		open(name, options);
 	}
-
-	private JComboBox optionsBox;
-
-	private JPanel createMainPanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-
-		JPanel addPanel = new JPanel();
-		addPanel.setLayout(new BoxLayout(addPanel, BoxLayout.X_AXIS));
-
-		JLabel label = new JLabel("Selected:");
-		addPanel.add(label);
-
-		optionsBox = new JComboBox<>(options);
-		optionsBox.setMaximumSize(new Dimension(Integer.MAX_VALUE, optionsBox.getPreferredSize().height));
-
-		addPanel.add(optionsBox);
-
-		panel.add(addPanel);
-
-		JButton addButton = new JButton("Click to remove");
-		addButton.addActionListener(event -> {
-			onRemove((String) optionsBox.getSelectedItem());
-			frame.dispose();
-		});
-		panel.add(addButton);
-
-		return panel;
+	
+	public RemoveEditor(String name, String... options) {
+		open(name, Arrays.asList(options));
 	}
 
 	public abstract void onRemove(String name);
 
-	// public abstract void saveChanges(Map<String, Object> changes);
 }
