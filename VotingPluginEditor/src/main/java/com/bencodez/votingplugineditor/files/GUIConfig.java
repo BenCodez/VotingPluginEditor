8201336c71cfb5e4e11362c5e649cfd30e862ab4
Ben <benbergen12@gmail.com>
2026-07-18 11:49:35 -0600

    Reconcile VoteURL editor with merged GUI updates

--- VotingPluginEditor/src/main/java/com/bencodez/votingplugineditor/files/GUIConfig.java
+++ VotingPluginEditor/src/main/java/com/bencodez/votingplugineditor/files/GUIConfig.java
@@ -61,26 +61,19 @@ public class GUIConfig extends YmlConfigHandler {
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 		Map<String, Object> data = getConfigData();
+		String[] standardMethods = { "CHAT", "CHEST", "DIALOG" };
 
 		panel.add(PanelUtils.createSectionLabel("GUIMethod"));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Today", data, "Today GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.TopVoter", data, "TopVoter GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Last", data, "Last GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Next", data, "Next GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Total", data, "Total GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Today", data, "Today GUI Method", "CHEST", standardMethods));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.TopVoter", data, "TopVoter GUI Method", "CHEST", standardMethods));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Last", data, "Last GUI Method", "CHEST", standardMethods));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Next", data, "Next GUI Method", "CHEST", standardMethods));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Total", data, "Total GUI Method", "CHEST", standardMethods));
 		settingButtons.add(new StringSettingButton(panel, "GUIMethod.URL", data, "VoteURL GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST", "BOOK" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Best", data, "Best GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Streak", data, "Streak GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
-		settingButtons.add(new StringSettingButton(panel, "GUIMethod.GUI", data, "Main GUI Method", "CHEST",
-				new String[] { "CHAT", "CHEST" }));
+				new String[] { "CHAT", "CHEST", "BOOK", "DIALOG" }));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Best", data, "Best GUI Method", "CHEST", standardMethods));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.Streak", data, "Streak GUI Method", "CHEST", standardMethods));
+		settingButtons.add(new StringSettingButton(panel, "GUIMethod.GUI", data, "Main GUI Method", "CHEST", standardMethods));
 
 		settingButtons.add(new BooleanSettingButton(panel, "LastMonthGUI", data, "Enable LastMonthGUI:"));
 
@@ -126,10 +119,8 @@ public class GUIConfig extends YmlConfigHandler {
 
 		panel.add(createItemEditorButton("CHEST.VoteURL.AlreadyVotedItem", "Edit Already Voted Item"));
 		panel.add(createItemEditorButton("CHEST.VoteURL.CanVoteItem", "Edit Can Vote Item"));
-		panel.add(createItemEditorButton("CHEST.VoteURL.AllUrlsButton.AlreadyVotedItem",
-				"Edit All URLs Already Voted Item"));
-		panel.add(createItemEditorButton("CHEST.VoteURL.AllUrlsButton.CanVoteItem",
-				"Edit All URLs Can Vote Item"));
+		panel.add(createItemEditorButton("CHEST.VoteURL.AllUrlsButton.AlreadyVotedItem", "Edit All URLs Already Voted Item"));
+		panel.add(createItemEditorButton("CHEST.VoteURL.AllUrlsButton.CanVoteItem", "Edit All URLs Can Vote Item"));
 
 		PanelUtils.adjustSettingButtonsMaxWidth(buttons);
 		frame.add(panel, BorderLayout.CENTER);
@@ -172,12 +163,9 @@ public class GUIConfig extends YmlConfigHandler {
 
 		Map<String, Object> backButtonData = (Map<String, Object>) get("CHEST.BackButton", new HashMap<String, Object>());
 		ArrayList<SettingButton> buttons = new ArrayList<SettingButton>();
-		buttons.add(new BooleanSettingButton(panel, "CHEST.BackButton.OpenVoteURL", getConfigData(),
-				"Open VoteURL when clicked"));
-		buttons.add(new BooleanSettingButton(panel, "CHEST.BackButton.EndOfGUI", getConfigData(),
-				"Place at end of GUI"));
-		buttons.add(new BooleanSettingButton(panel, "CHEST.BackButton.Exit", getConfigData(),
-				"Use as exit button"));
+		buttons.add(new BooleanSettingButton(panel, "CHEST.BackButton.OpenVoteURL", getConfigData(), "Open VoteURL when clicked"));
+		buttons.add(new BooleanSettingButton(panel, "CHEST.BackButton.EndOfGUI", getConfigData(), "Place at end of GUI"));
+		buttons.add(new BooleanSettingButton(panel, "CHEST.BackButton.Exit", getConfigData(), "Use as exit button"));
 		settingButtons.addAll(buttons);
 
 		JButton editItem = new JButton("Edit Back Button Item");
@@ -200,7 +188,6 @@ public class GUIConfig extends YmlConfigHandler {
 
 		PanelUtils.adjustSettingButtonsMaxWidth(buttons);
 		frame.add(panel, BorderLayout.CENTER);
-
 		JButton saveButton = new JButton("Save and Apply Changes");
 		saveButton.addActionListener(e -> saveChanges());
 		frame.add(saveButton, BorderLayout.SOUTH);
@@ -216,7 +203,6 @@ public class GUIConfig extends YmlConfigHandler {
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
 		Map<String, Object> map = (Map<String, Object>) get("CHEST.VoteGUI", new HashMap<String, Object>());
 
 		AddRemoveEditor addRemoveEditor = new AddRemoveEditor(frame.getWidth()) {
@@ -262,8 +248,7 @@ public class GUIConfig extends YmlConfigHandler {
 		};
 
 		panel.add(addRemoveEditor.getAddButton("Add Item/Slot", "Add Item/Slot"));
-		panel.add(addRemoveEditor.getRemoveButton("Remove Item/Slot", "Remove Item/Slot",
-				PanelUtils.convertSetToArray(map.keySet())));
+		panel.add(addRemoveEditor.getRemoveButton("Remove Item/Slot", "Remove Item/Slot", PanelUtils.convertSetToArray(map.keySet())));
 		addRemoveEditor.getOptionsButtons(panel, PanelUtils.convertSetToArray(map.keySet()));
 
 		frame.add(panel);
